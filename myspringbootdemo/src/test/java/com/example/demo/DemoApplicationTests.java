ed5eb1c45292eddd5f6289849fc5f70f69257cad
chenlin3 <chenlin3@allinpay.com>
2019-04-03 10:50:40 +0800

    create project

---
+++ myspringbootdemo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import com.example.demo.model.AyUser;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.lang.Override;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Resource
+    private JdbcTemplate jdbcTemplate;
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void mySqlTest(){
+        String sql="select id,name,password from ay_user";
+        List<AyUser> userList=(List<AyUser>)jdbcTemplate.query(sql, new RowMapper<AyUser>(){
+            @Override
+            public AyUser mapRow(ResultSet rs,int rowNum)throws SQLException {
+                AyUser user =new AyUser();
+                user.setId(rs.getString("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("Password"));
+                return user;
+            }
+        });
+        for (AyUser user:userList){
+            System.out.println("id:"+user.getId());
+            System.out.println("name:"+user.getName());
+            System.out.println("password:"+user.getPassword());
+        }
+    }
+}
