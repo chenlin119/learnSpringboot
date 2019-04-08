7f8831fc5154581c5dc51465f8f3d3ba0046601b
chenlin3 <chenlin3@allinpay.com>
2019-04-08 17:18:06 +0800

    ddd

---
+++ myspringbootdemo/src/main/java/com/example/demo/domain/MyLink.java
@@ -0,0 +1,40 @@
+package com.example.demo.domain;
+
+public class MyLink {
+    private int id;
+    private String name;
+    private String link;
+    private int status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
