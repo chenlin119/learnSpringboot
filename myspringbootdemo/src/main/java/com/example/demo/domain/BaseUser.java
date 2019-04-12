ed447a551e130dfd124f240b3a8a8b1f8caeb079
chenlin3 <chenlin3@allinpay.com>
2019-04-12 10:03:42 +0800

    redis

--- myspringbootdemo/src/main/java/com/example/demo/domain/BaseUser.java
+++ myspringbootdemo/src/main/java/com/example/demo/domain/BaseUser.java
@@ -1,6 +1,8 @@
 package com.example.demo.domain;
 
-public class BaseUser {
+import java.io.Serializable;
+
+public class BaseUser implements Serializable {
     private int id;
     private String username;
     private int age;
--- myspringbootdemo/src/main/java/com/example/demo/service/impl/BaseUserServiceImpl.java
+++ myspringbootdemo/src/main/java/com/example/demo/service/impl/BaseUserServiceImpl.java
@@ -3,17 +3,46 @@ package com.example.demo.service.impl;
 import com.example.demo.dao.master.BaseUserDao;
 import com.example.demo.domain.BaseUser;
 import com.example.demo.service.BaseUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class BaseUserServiceImpl implements BaseUserService {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseUserServiceImpl.class);
     @Autowired
     private BaseUserDao userDao;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     
     public BaseUser getUserById(int id) {
-        return userDao.getUserById(id);
+        // 从缓存中获取城市信息
+        String key = "user_" + id;
+        ValueOperations<String, BaseUser> operations = redisTemplate.opsForValue();
+
+        // 缓存存在
+        boolean hasKey = redisTemplate.hasKey(key);
+        if (hasKey) {
+            BaseUser user = operations.get(key);
+
+            LOGGER.info("BaseUserServiceImpl.findCityById() : 从缓存中获取了城市 >> " + user.toString());
+            return user;
+        }
+
+        // 从 DB 中获取城市信息
+        BaseUser user = userDao.getUserById(id);
+
+        // 插入缓存
+
+        operations.set(key, user);
+        LOGGER.info("BaseUserServiceImpl.findCityById() : 城市插入缓存 >> " + user.toString());
+        return user;
     }
 }
